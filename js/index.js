c3f29c2839b4fd3197ca070bc3c3d1f1e9a7f009
unknown <you@example.com>
2019-01-08 20:04:12 +0800

    pb

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(function(){
 	$(window).resize(function(){
 		var cliwidth=document.body.clientWidth;
-		var divwidth=cliwidth-2;
+		var divwidth2=cliwidth/2;
 		$('music').css("width",divwidth+"px");
 	})
 })
