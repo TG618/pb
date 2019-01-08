b016de627b19613fd046558e491cb397fa08f110
TG618 <46296820+TG618@users.noreply.github.com>
2019-01-08 21:11:49 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 $(function(){
 	$(window).resize(function(){
 		var cliwidth=document.body.clientWidth;
-		var divwidth2=cliwidth/2;
+		var divwidth=cliwidth-2;
 		$('music').css("width",divwidth+"px");
+	})
+})
+$(function(){
+	$(window).resize(function(){
+		var cliheight=document.body.clientHeight;
+		var divheight=cliheight-10;
+		$('text .textcell').css("height",divheight+"px");
 	})
 })
